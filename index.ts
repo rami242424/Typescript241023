eccd6a7423802c471b5a78859623a7b328114aad
Rami <138556019+rami242424@users.noreply.github.com>
2024-10-28 14:33:26 +0900

    3.2

--- index.ts
+++ index.ts
@@ -1,16 +1,9 @@
-type Config = {
-    path: string,
-    state: object
+type Add = {
+    (a: number, b: number) : number
+    (a: number, b: number, c: number) : number
 }
 
-type Push = {
-    (path:string) : void
-    (config : Config): void
-}
-
-const push:Push = (config) => {
-    if(typeof config === "string") {console.log(config)}
-    else {
-        console.log(config.path)
-    }
+const add : Add = (a,b,c?:number) =>  {
+    if(c) return a + b + c
+    return a + b
 }
