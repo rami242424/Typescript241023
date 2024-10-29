ade46913ab46ff8c21e6a898a507854731f0a8e6
Rami <138556019+rami242424@users.noreply.github.com>
2024-10-29 11:37:01 +0900

    3.3

--- index.ts
+++ index.ts
@@ -1,11 +1,9 @@
 // generic
-type SuperPrint = {
-    <TypePlaceHolder>(arr: TypePlaceHolder[]) : TypePlaceHolder
-}
+type SuperPrint = <T, M>(a: T[], b: M) => T
 
 const superPrint : SuperPrint = (arr) => arr[0]
 
-const a = superPrint([1,2,3,4])
-const b = superPrint([true, false, false])
-const c = superPrint(["string", "why"])
-const d = superPrint([1, false, "yay"])
+const a = superPrint([1,2,3,4], "x")
+const b = superPrint([true, false, false], [8])
+const c = superPrint(["string", "why"], true)
+const d = superPrint([1, false, "yay"], ["stringtoo", "stringone"])
