16c5bff62eae1b58ce5076d02321f213d7caa304
Rami <138556019+rami242424@users.noreply.github.com>
2024-10-29 19:31:54 +0900

    #4.2 Interfaces

--- index.ts
+++ index.ts
@@ -1,36 +1,24 @@
-type Words = {
-    [key:string]: string
+// 기본적인 type 사용방법(obj 형식)
+// type Player = {
+//     nickname: string,
+//     healthBar: number
+// }
+
+// const nico : Player = {
+//  nickname: "nico",
+//  healthBar: 10
+// }
+
+// type의 다른 사용방법(타입 alias 대체명 형식)
+type Food = string;
+const kimchi : Food = "delicious"
+
+
+//
+type Nickname = string;
+type HealthBar = number;
+type Friends = Array<string>
+type Player = {
+    nickname : Nickname,
+    healthBar : HealthBar
 }
-
-class Dict {
-    private words: Words
-    constructor(){
-        this.words = {}
-    }
-    add(word:Word){
-        if(this.words[word.term] === undefined){
-            this.words[word.term] = word.def
-        }
-    }
-    def(term:string){
-        return this.words[term]
-    }
-    static hello(){
-        return "hello";
-    }
-}
-
-class Word {
-    constructor(
-        public readonly term : string,
-        public readonly def : string
-    ){}
-}
-
-const kimchi = new Word("kimchi", "한국의 음식");
-
-const dict = new Dict()
-dict.add(kimchi);
-dict.def("kimchi");
-
-Dict.hello()
