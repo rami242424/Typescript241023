6e72f5bead76973448b37b062cc0b8e4e01ca732
Rami <138556019+rami242424@users.noreply.github.com>
2024-10-29 22:24:55 +0900

    #4.2 Interfaces

--- index.ts
+++ index.ts
@@ -1,30 +1,24 @@
-// type : 원하는 모든 값이 될 수 있음
-// interface : 오직 한가지 용도로만 사용 - 오브젝트의 모양을 특정해주기 위해 사용
-
-type Team = "red" | "blue" | "yellow"
-// type Team = string
-type Health = 1 | 5 | 10
-// type Health = number
-// type Player = {
-//     nickname : string
-//     team : Team
-//     health: Health
-// }
-
-
-interface Player {
-    nickname : string
-    team : Team
-    health: Health
+// type
+type UUser = {
+    name: string
 }
-// interface Person = "nico"|"lynn" // X
+type PPlayer = UUser & {
 
+}
 
+// interface
+interface User {
+    name: string
+}
+interface Player extends User{   
+}
 
-
-
+const nnico : PPlayer = {
+    name: "nicoo"
+}
 const nico : Player = {
-    nickname: "nico",
-    team: "yellow",
-    health: 10
+    name: "nico"
 }
+
+nico.name = "lalla"
+nnico.name = "babab"
