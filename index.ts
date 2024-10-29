1383578c9b6d8b1553d3b798d9f74e407049d3aa
Rami <138556019+rami242424@users.noreply.github.com>
2024-10-29 22:19:50 +0900

    #4.2 Interfaces

--- index.ts
+++ index.ts
@@ -1,24 +1,30 @@
-// 기본적인 type 사용방법(obj 형식)
+// type : 원하는 모든 값이 될 수 있음
+// interface : 오직 한가지 용도로만 사용 - 오브젝트의 모양을 특정해주기 위해 사용
+
+type Team = "red" | "blue" | "yellow"
+// type Team = string
+type Health = 1 | 5 | 10
+// type Health = number
 // type Player = {
-//     nickname: string,
-//     healthBar: number
+//     nickname : string
+//     team : Team
+//     health: Health
 // }
 
-// const nico : Player = {
-//  nickname: "nico",
-//  healthBar: 10
-// }
 
-// type의 다른 사용방법(타입 alias 대체명 형식)
-type Food = string;
-const kimchi : Food = "delicious"
+interface Player {
+    nickname : string
+    team : Team
+    health: Health
+}
+// interface Person = "nico"|"lynn" // X
 
 
-//
-type Nickname = string;
-type HealthBar = number;
-type Friends = Array<string>
-type Player = {
-    nickname : Nickname,
-    healthBar : HealthBar
+
+
+
+const nico : Player = {
+    nickname: "nico",
+    team: "yellow",
+    health: 10
 }
