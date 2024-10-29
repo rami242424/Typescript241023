f49a7252a13772827311d0a320b2f33d5bffd513
Rami <138556019+rami242424@users.noreply.github.com>
2024-10-29 11:56:59 +0900

    #3.4 Conclusions

--- index.ts
+++ index.ts
@@ -1,44 +1,7 @@
-// generic 이론
-// type SuperPrint = <T, M>(a: T[], b: M) => T
+// generic 사용방법2 : Array<number>
+type a = Array<number>
+let a : A = [1, 2, 3, 4]
 
-// const superPrint : SuperPrint = (arr) => arr[0]
 
-// const a = superPrint([1,2,3,4], "x")
-// const b = superPrint([true, false, false], [8])
-// const c = superPrint(["string", "why"], true)
-// const d = superPrint([1, false, "yay"], ["stringtoo", "stringone"])
-
-// generic의 실제사용방식1
-// function superPrint<T>(a: T[]){
-//     return a[0]
-// }
-
-// const a = superPrint<number>([1,2,3,4])
-// const b = superPrint([true, false, false])
-// const c = superPrint(["string", "why"])
-// const d = superPrint([1, false, "yay"])
-
-// generic의 실제사용방식2
-type Player<E> = {
-    name: string
-    extraInfo: E
-}
-
-// 아래 3개의 코드는 다 같다.
-type NicoExtra = {
-    favFood : string;
-}
-// type NicoPlayer = Player<{favFood:string}> 또는 위에 코드 사용해서 아래와 같이 표현할 수 있다.
-type NicoPlayer = Player<NicoExtra>
-
-const nico : Player<{favFood:string}> = {
-    name: "nico",
-    extraInfo: {
-        favFood: "kimchi"
-    }
-}
-
-const lynn: Player<null> = {
-    name: "lynn",
-    extraInfo: null
+function printAllNumber(arr: Array<number>){
 }
