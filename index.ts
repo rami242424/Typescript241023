c6d9823b56a97e2cee15dd92d0eab586b8748b11
Rami <138556019+rami242424@users.noreply.github.com>
2024-10-29 11:42:43 +0900

    #3.4 Conclusions

--- index.ts
+++ index.ts
@@ -1,9 +1,19 @@
-// generic
-type SuperPrint = <T, M>(a: T[], b: M) => T
+// generic 이론
+// type SuperPrint = <T, M>(a: T[], b: M) => T
 
-const superPrint : SuperPrint = (arr) => arr[0]
+// const superPrint : SuperPrint = (arr) => arr[0]
 
-const a = superPrint([1,2,3,4], "x")
-const b = superPrint([true, false, false], [8])
-const c = superPrint(["string", "why"], true)
-const d = superPrint([1, false, "yay"], ["stringtoo", "stringone"])
+// const a = superPrint([1,2,3,4], "x")
+// const b = superPrint([true, false, false], [8])
+// const c = superPrint(["string", "why"], true)
+// const d = superPrint([1, false, "yay"], ["stringtoo", "stringone"])
+
+// generic의 실제사용방식
+function superPrint<T>(a: T[]){
+    return a[0]
+}
+
+const a = superPrint<number>([1,2,3,4])
+const b = superPrint([true, false, false])
+const c = superPrint(["string", "why"])
+const d = superPrint([1, false, "yay"])
