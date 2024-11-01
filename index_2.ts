50d4291c583fefa4b266c84ecbc39c23ab782297
Rami <138556019+rami242424@users.noreply.github.com>
2024-11-02 00:12:36 +0900

    2.2

--- index_2.ts
+++ index_2.ts
@@ -5,12 +5,17 @@ type Player = {
     age?: Age
 }
 
-function playerMaker(name: string, age: number){
-    return {
-        name,
-        age
-    }
-}
+// function playerMaker(name: string, age: number){
+//     return {
+//         name,
+//         age
+//     }
+// }
+// 화살표함수
+const playerMaker(name:string, age:number) => ({
+    name,
+    age
+})
 
 const nico = playerMaker("nico", 12)
 nico.age = 20
