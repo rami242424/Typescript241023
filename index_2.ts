6ca7ae231698748202a4a3ccea666e933644ff18
Rami <138556019+rami242424@users.noreply.github.com>
2024-11-05 20:38:17 +0900

    2.4 never -1

--- index_2.ts
+++ index_2.ts
@@ -1,6 +1,6 @@
 // never : 함수가 절대 return하지 않을 때 발생
 
-// Type 'string' is not assignable to type 'never'
+// return 하지 않고 오류를 발생시키는 함수
 function hello(): never{
-    return "x"
+    throw new Error("X")
 } 
