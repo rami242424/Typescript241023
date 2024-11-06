5c995128e42efdb294fbe63199ece3ea6606e9da
Rami <138556019+rami242424@users.noreply.github.com>
2024-11-06 16:43:50 +0900

    #3.4 Conclusions

--- index_2.ts
+++ index_2.ts
@@ -1,9 +1,13 @@
-type SuperPrint = {
-    <T>(arr: T[]): T
+// type SuperPrint = {
+//     <T, M>(a: T[], b: M): T
+// }
+// const superPrint: SuperPrint = (arr) => arr[0]
+
+// 위코드 통합가능(아래와 같이)
+function superPrint<V, T>(a: V[], b: T){
+    return a[0]
 }
 
-const superPrint: SuperPrint = (arr) => arr[0]
-
-const a = superPrint([1,2,3,4])
-const b = superPrint([true, false, false])
-const c = superPrint([false, true, 2, "yay", 4])
+const a = superPrint([1,2,3,4], true)
+const b = superPrint([true, false, false], 2)
+const c = superPrint([false, true, 2, "yay", 4], "wow")
