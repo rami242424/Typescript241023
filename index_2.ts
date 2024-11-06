3de8f611a02437c97ba1d8e3ad860529da1f5ec0
Rami <138556019+rami242424@users.noreply.github.com>
2024-11-06 21:32:17 +0900

    4.0 classes

--- index_2.ts
+++ index_2.ts
@@ -1,21 +1,17 @@
-type Player<E> = {
-    name: string
-    extraInfo: E
+// abstract class (추상클래스) : 다른 클래스가 상속 받을 수 있는 클래스 -> 다른곳에서 상속받을 수만 있고, 직접적으로 새로운 인스턴스를 만들 수는 없다.
+
+abstract class User {
+    constructor(
+        private firstName: string,
+        private lastName: string,
+        public nickName: string
+    ) {}
 }
 
-type NicoExtra = {
-    favFood: string;
-}
-type NicoPlayer = Player<NicoExtra>
 
-const nico : NicoPlayer = {
-    name: "nico",
-    extraInfo: {
-        favFood: "hamburger"
-    }
+class Player extends User {
 }
 
-const lynn : Player<null> = {
-    name: "lynn",
-    extraInfo: null
-}
+const nico = new Player("nico", "las", "니꼬");
+
+
