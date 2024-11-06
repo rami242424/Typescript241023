e5fccf32c450a4bb6c46fd4b7419ce00349f64bc
Rami <138556019+rami242424@users.noreply.github.com>
2024-11-06 16:15:27 +0900

    Overloading

--- index_2.ts
+++ index_2.ts
@@ -1,3 +1,17 @@
-// call signature
-type Add = (a:number, b:number) => number
-const add: Add = (a, b) => a + b
+// overloading : 함수가 여러개의 call signature을 가지고 있을 때 발생
+type Config = {
+    path: string,
+    state: object
+}
+
+type Push = {
+    (path: string): void
+    (config: Config): void
+}
+
+const push: Push = (config) => {
+    if(typeof config === "string") { console.log(config) }
+    else {
+        console.log(config.path)
+    }
+}
