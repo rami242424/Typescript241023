da05ba7e6ce99e9668b9b32b50d1a953cd95f98c
Rami <138556019+rami242424@users.noreply.github.com>
2024-11-07 20:10:32 +0900

    4.2 interfaces

--- index_2.ts
+++ index_2.ts
@@ -1,37 +1,16 @@
-type Words = {
-    [key:string]:string
+type Nickname = string
+type Health = number
+type Friends = Array<string>
+
+type Player = {
+    nickname: Nickname,
+    healthBar: Health
 }
 
-
-class Dict {
-    private words: Words
-    constructor(){
-        this.words = {}
-    }
-    add(word:Word){
-        if(this.words[word.term] === undefined){
-            this.words[word.term] = word.def;
-        }
-    }
-    def(term:string){
-        return this.words[term]
-    }
-    static hello(){
-        return "hello";
-    }
+const nico: Player = {
+    nickname:"nico",
+    healthBar: 10
 }
 
-class Word {
-    constructor(
-        public readonly term : string,
-        public readonly def: string
-    ){}
-}
-
-const kimchi = new Word("kimchi", "한국의 음식");
-
-const dict = new Dict();
-dict.add(kimchi);
-dict.def("kimchi")
-
-Dict.hello()
+type Food = string;
+const kimchi : Food = "delicious"
