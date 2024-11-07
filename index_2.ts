63374824822acc9eb19a6b9e8532c6884000c0d2
Rami <138556019+rami242424@users.noreply.github.com>
2024-11-07 21:25:38 +0900

    #4.3 Interfaces part Two

--- index_2.ts
+++ index_2.ts
@@ -1,17 +1,25 @@
 interface User {
-    name: string
+    firstName: string,  
+    lastName: string
+    sayHi(name: string): string
+    fullName(): string
 }
 
-interface User {
-    firstName: string
+// 추가
+interface Human {
+    health: number
 }
 
-interface User {
-    health : number
-}
-
-const nico : User = {
-    name: "nico",
-    firstName: "oh",
-    health : 3
+class Player implements User {
+    constructor(
+        public firstName:string,
+        public lastName: string,
+        public health: number
+    ){}
+    fullName(){
+        return `${this.firstName} ${this.lastName}`
+    }
+    sayHi(name: string){
+        return `Hello ${name}. My name is ${this.firstName}`
+    }
 }
