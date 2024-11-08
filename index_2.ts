ef94594cb04fab761414ad127178a4f857c6364a
Rami <138556019+rami242424@users.noreply.github.com>
2024-11-08 18:26:30 +0900

    3.1

--- index_2.ts
+++ index_2.ts
@@ -1,3 +1,11 @@
+// call signature - shortcut
 type Add = (a:number, b:number) => number;
 
-const add : Add = (a, b) => a + b
+// call signature - long way
+type Add = {
+    (a:number, b:number) : number
+}
+
+const add : Add = (a, b) => a + b 
+
+// 오버로딩 : 서로 다른 여러개의 call signature을 가졌을 때 발생
