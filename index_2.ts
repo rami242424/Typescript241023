557b8ddfd0d1514356be1d0b06f0619e70975aca
Rami <138556019+rami242424@users.noreply.github.com>
2024-11-08 18:56:23 +0900

    3.2

--- index_2.ts
+++ index_2.ts
@@ -1,9 +1,10 @@
-type Add = {
-    (a:number, b:number) : number
-    (a:number, b:number, c:number) : number
+type SuperPrint = {
+    <T>(arr: T[]) : T
 }
 
-const add : Add = (a, b, c?:number) => {
-    if(c) return a + b + c
-    return a + b
-}
+const superPrint: SuperPrint = (arr) => arr[0]
+
+const a = superPrint([1,2,3])
+const b = superPrint([false, false, true])
+const c = superPrint(["yay", "wow", true])
+const d = superPrint([1,2,3, true, "yay", 2])
