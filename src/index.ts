379e0dcd0d8c40560a1854f7d1d5ba803d9d0032
Rami <138556019+rami242424@users.noreply.github.com>
2024-11-08 11:59:58 +0900

    final

--- src/index.ts
+++ src/index.ts
@@ -76,3 +76,32 @@ class Block implements BlockShape{
         return crypto.createHash("sha256").update(toHash).digest("hex");
     }
 }
+
+class Blockchain {
+    private blocks: Block[]
+    constructor(){
+        this.blocks = [];
+    }
+    private getPrevHash(){
+        if(this.blocks.length === 0) return "";
+        return this.blocks[this.blocks.length - 1].hash;
+    }
+    public addBlock(data:string){
+        const newBlock = new Block(this.getPrevHash(), this.blocks.length + 1, data)
+        this.blocks.push(newBlock);
+    }
+    public getBlocks(){
+        // return this.blocks; // 해킹될수있다.
+        return [...this.blocks];
+    }
+}
+
+const blockchain = new Blockchain();
+
+blockchain.addBlock("first one");
+blockchain.addBlock("second one");
+blockchain.addBlock("third one");
+
+blockchain.getBlocks().push(new Block("yay", 454645465, "i got this")) // return this.blocks; 코드를 짜면 이렇게 해킹가능
+
+console.log(blockchain.getBlocks());
